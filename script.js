91d0e2ef3411c5ccd07a7d93eff952e01eb4a636
gottbrecht <simo79j5@stud.kea.dk>
2024-03-10 19:20:37 +0100

    remove

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ class CircularQueue {
             this.writeIndex = (this.writeIndex + 1) % this.size;
         }
     }
+
+    remove() {
+        if (this.readIndex === this.writeIndex) {
+            console.log("Queue is empty. Cannot remove more items.");
+            return null;
+        } else {
+            const item = this.buffer[this.readIndex];
+            this.readIndex = (this.readIndex + 1) % this.size;
+            return item;
+        }
+    }
 }
