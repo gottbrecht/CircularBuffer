7b8710007392de047c72bab159a91c7aec5426cd
gottbrecht <simo79j5@stud.kea.dk>
2024-03-10 19:18:27 +0100

    add(item)

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ class CircularQueue {
         this.readIndex = 0;
         this.writeIndex = 0;
     }
+
+    add(item) {
+
+        if ((this.writeIndex + 1) % this.size === this.readIndex) {
+            console.log("Queue is full. Cannot add more items.");
+        } else {
+            this.buffer[this.writeIndex] = item;
+            this.writeIndex = (this.writeIndex + 1) % this.size;
+        }
+    }
+}
